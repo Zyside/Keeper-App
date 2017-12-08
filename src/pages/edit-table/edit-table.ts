55cc44d8c57d8227163a3817613a7fc902fb2553
dobriy <o.sirivlya@gmail.com>
2017-12-08 14:44:45 +0200

    Edit-table fix count method

--- src/pages/edit-table/edit-table.ts
+++ src/pages/edit-table/edit-table.ts
@@ -9,134 +9,224 @@ import { NavController, NavParams } from 'ionic-angular';
 export class EditTablePage {
 
   table: object[];
+  // totalCount:number;
   showBar: boolean = false;
   showHookah:boolean = false;
   showKitchen:boolean = false;
   showArray: object[] = [];
+  alcohol: any = {};
+  alcoholKeys: string[] = [];
+  kitchen:any = {};
+  kitchenKeys:string[]=[];
+  hookah:any={};
+  hookahKeys:string[] = [];
 
-  french:object[];
-  japan:object[];
-  launch:object[];
-  roms: object[];
-  vodkas: object[];
-  whiskeys: object[];
   totalScore: object[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public actionSheetCtrl: ActionSheetController) {
     this.table = this.navParams.get('item');
-    console.log('parvm table------>', this.table);
-    this.whiskeys = [
-      {
-        name: 'Jack Daneils',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Jim Beam',
-        price: 65,
-        volume: 0.04
-      },
-      {
-        name: 'Jameson',
-        price: 60,
-        volume: 0.04
-      }
-    ];
-    this.roms = [
-      {
-        name: 'Okharte',
-        price: 50,
-        volume: 0.04
-      },
-      {
-        name: 'Bacardi',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Capitan Morgan',
-        price: 45,
-        volume: 0.04
-      }
-    ];
-    this.vodkas = [
-      {
-        name: 'Finland',
-        price: 50,
-        volume: 0.04
-      },
-      {
-        name: 'Green Day',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Zubrovka',
-        price: 45,
-        volume: 0.04
-      }
-    ];
+    this.kitchen = {
+      'Японская' : [
+        {
+          name: 'Рол',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Мидии',
+          price: 65,
+          count: 1
+        },
+        {
+          name: 'Красная рыба',
+          price: 60,
+          count: 1
+        }
+      ],
+      'Франция': [
+        {
+          name: 'Круасан',
+          price: 50,
+          count: 1
+        },
+        {
+          name: 'Улитки',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Фондю',
+          price: 45,
+          count: 1
+        }
+      ],
+      'Америка': [
+        {
+          name: 'Стейк медиум',
+          price: 50,
+          count: 1
+        },
+        {
+          name: 'Стейк куриный',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Стейк из говядины',
+          price: 45,
+          count: 1
+        }
+      ]
+    };
+    this.kitchenKeys = Object.keys(this.kitchen);
 
-    this.french = [
-      {
-        name: 'kruasan',
-        price: 50,
-        volume: 0.04
-      },
-      {
-        name: 'Fondu',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Улитки',
-        price: 45,
-        volume: 0.04
-      }
-    ];
-    this.japan = [
-      {
-        name: 'Рол Калифорния',
-        price: 50,
-        volume: 0.04
-      },
-      {
-        name: 'Рис',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Красная рыба',
-        price: 45,
-        volume: 0.04
-      }
-    ];
-    this.launch = [
-      {
-        name: 'Салат',
-        price: 50,
-        volume: 0.04
-      },
-      {
-        name: 'Жаренные яйца',
-        price: 70,
-        volume: 0.04
-      },
-      {
-        name: 'Блинчики',
-        price: 45,
-        volume: 0.04
-      }
-    ];
+    this.alcohol = {
+      'Виски' : [
+        {
+          name: 'Jack Daneils',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Jim Beam',
+          price: 65,
+          count: 1
+        },
+        {
+          name: 'Jameson',
+          price: 60,
+          count: 1
+        }
+      ],
+      'Ромы': [
+        {
+          name: 'Okharte',
+          price: 50,
+          count: 1
+        },
+        {
+          name: 'Bacardi',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Capitan Morgan',
+          price: 45,
+          count: 1
+        }
+      ],
+      'Водка': [
+        {
+          name: 'Finland',
+          price: 50,
+          count: 1
+        },
+        {
+          name: 'Green Day',
+          price: 70,
+          count: 1
+        },
+        {
+          name: 'Zubrovka',
+          price: 45,
+          count: 1
+        }
+      ]
+    };
+    this.alcoholKeys = Object.keys(this.alcohol);
 
+    this.hookah = {
+
+      'Классика':[
+        {
+        name:'Шербет',
+        price:140,
+          count: 1
+      },
+        {
+          name:'Adalia',
+          price:180,
+          count: 1
+        },
+        {
+          name:'Nahla',
+          price:180,
+          count: 1
+        },
+      ],
+      'Премиум':[
+        {
+        name:'Fumari',
+        price:200,
+          count: 1
+      },
+        {
+          name:'Танж',
+          price:200,
+          count: 1
+        },
+        {
+          name:'DakrSide',
+          price:200,
+          count: 1
+        }
+      ]
+    };
+    this.hookahKeys = Object.keys(this.hookah);
   }
 
 
-  addToTotalScore(item) {
-    this.totalScore.push(item);
-      this.table['order'] = this.totalScore;
+  addToTotalScore(linkItem) {
+    let item = JSON.parse(JSON.stringify(linkItem))
+    let index = this.checkItem(item, this.totalScore);
+    console.log('item', item);
+    console.log(index)
+    if  (index!==-1) {
+      this.totalScore[index]['count'] += 1;
+      this.totalScore[index]['price'] += item['price'];
+    } else {
+      this.totalScore.push(item);
+    }
+    this.table['order'] = this.totalScore;
+    console.log('totalScore', this.totalScore)
+    }
+
+
+  checkItem(item, array) {
+    console.log('check item');
+    for(let i=0; i<array.length; i++){
+      console.log('length', array.length)
+      console.log('i', i)
+      if (array[i].name === item.name){
+        return i
+      }
+    }
+    return -1
   }
+  //
+  // changeCount(linkItem){
+  //   let item = JSON.parse(JSON.stringify(linkItem))
+  //   let index = this.checkItem(item, this.totalScore);
+  //   console.log('item', item);
+  //   console.log(index)
+  //   if  (index!==-1) {
+  //     this.totalScore[index]['count'] = this.totalCount;
+  //     this.totalScore[index]['price']= ((this.totalScore[index]['price'] *10) * (this.totalScore[index]['count']*10))/10;
+  //   }
+  // }
+
+  removeItem(item){
+    if((item['price'] != 0) && (item['count'] != 0) &&(item['price'] != 1) && (item['count'] != 1)){
+      item['price'] = item['price'] - (item['price'] / item['count']);
+      item['count'] -= 1;
+      // console.log('remove item', item);
+      // console.log(this.totalScore);
+    } else if (item['count'] <= 1){
+      this.deleteItem(item);
+      }
+    }
+
 
   getResultPrice() {
     return this.totalScore.reduce((prev, el) => {
@@ -153,38 +243,16 @@ export class EditTablePage {
     return false;
   }
 
-  addItem(item: string) {
-    if (item === 'whiskeyModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.whiskeys.length; i++) {
-        this.showArray.push(this.whiskeys[i]);
-      }
-    } else if (item === 'romsModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.roms.length; i++) {
-        this.showArray.push(this.roms[i]);
-      }
-    } else if (item === 'vodkaModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.vodkas.length; i++) {
-        this.showArray.push(this.vodkas[i]);
-      }
-    } else if (item === 'japanModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.japan.length; i++) {
-        this.showArray.push(this.japan[i]);
-      }
-    } else if (item === 'frenchModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.french.length; i++) {
-        this.showArray.push(this.french[i]);
-      }
-    } else if (item === 'launchModel') {
-      this.showArray.splice(0, this.showArray.length);
-      for (let i = 0; i < this.launch.length; i++) {
-        this.showArray.push(this.launch[i]);
-      }
-    }
+  addAlcohol(item: string) {
+    this.showArray = this.alcohol[item];
+  }
+
+  addKitchen(item: string) {
+    this.showArray = this.kitchen[item];
+  }
+
+  addHookah(item:string){
+    this.showArray = this.hookah[item];
   }
 
   test(){
